f1e195770127017d8ce0cf3c132ec1028cbff33d
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-08 18:52:48 +0100

    feat: initial setup of allane customer service

---
+++ allane-customer/src/test/java/com/allane/leasing/allanecustomer/AllaneCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allane.leasing.allanecustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllaneCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

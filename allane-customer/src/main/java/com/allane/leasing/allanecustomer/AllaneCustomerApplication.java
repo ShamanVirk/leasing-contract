545e63f87490201519ee191b558f8eac239a8e01
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-08 18:52:48 +0100

    feat: initial setup of allane customer service

---
+++ allane-customer/src/main/java/com/allane/leasing/allanecustomer/AllaneCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.allane.leasing.allanecustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaneCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllaneCustomerApplication.class, args);
+	}
+
+}

a4a1effe9b9fddafa8dc279ba59a29e5a2293355
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-11 01:12:28 +0100

    refactor: improve package naming (#8)

--- allane-leasing/src/main/java/com/allane/leasing/AllaneLeasingApplication.java
+++ allane-leasing/src/main/java/com/allane/leasing/AllaneLeasingApplication.java
@@ -1,4 +1,4 @@
-package com.allane.leasing.allaneleasing;
+package com.allane.leasing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class AllaneLeasingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AllaneLeasingApplication.class, args);
 	}
-
 }

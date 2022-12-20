a44769668e4252624df89ade7c4fd2def7da1e6d
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-20 19:36:54 +0100

    feat: implement get all customers

--- allane-leasing/src/main/java/com/allane/leasing/controller/service/impl/AllaneCustomersServiceImpl.java
+++ allane-leasing/src/main/java/com/allane/leasing/controller/service/impl/AllaneCustomersServiceImpl.java
@@ -1,5 +1,7 @@
 package com.allane.leasing.controller.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,15 +9,27 @@ import org.springframework.stereotype.Service;
 import com.allane.leasing.controller.service.AllaneCustomersService;
 import com.allane.leasing.model.CustomerPageResponse;
 import com.allane.leasing.model.PageRequest;
+import com.allane.leasing.persistent.entity.Customer;
+import com.allane.leasing.persistent.repository.CustomerRepository;
 
 @Service
 public class AllaneCustomersServiceImpl implements AllaneCustomersService {
     
+
     private static final Logger LOG = LoggerFactory.getLogger(AllaneCustomersServiceImpl.class);
     
+    private CustomerRepository customerRepository;
+    
+    public AllaneCustomersServiceImpl(CustomerRepository customerRepository) {
+        customerRepository = customerRepository;
+    }
+
     @Override
     public CustomerPageResponse getAllCustomers(PageRequest page) {
         LOG.info("Enter getAllCustomers");
-        return new CustomerPageResponse();
+        List<Customer> customers = customerRepository.findAll();
+        CustomerPageResponse customerPageResponse = new CustomerPageResponse();
+        customerPageResponse.setOverviewItems(customers);
+        return customerPageResponse;
     }
 }

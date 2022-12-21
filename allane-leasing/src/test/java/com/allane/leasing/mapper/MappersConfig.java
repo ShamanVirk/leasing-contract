dc5f5650eb46b0f6808b07d16c5cd86d84c4bab0
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-21 04:17:14 +0100

    feat: config mapstruct to access it generated class in tests

---
+++ allane-leasing/src/test/java/com/allane/leasing/mapper/MappersConfig.java
@@ -0,0 +1,10 @@
+package com.allane.leasing.mapper;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan("com.allane.leasing.mapper")
+public class MappersConfig {
+
+}

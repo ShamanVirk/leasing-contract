8615aa519a5c32a498314289b9de6d9adcf37340
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-21 05:46:29 +0100

    test: add integration tests for create customer and get paged customers (#18)

--- allane-leasing/src/test/java/com/allane/leasing/integration/AllaneCustomerIntegrationTests.java
+++ allane-leasing/src/test/java/com/allane/leasing/integration/AllaneCustomerIntegrationTests.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.allane.leasing.model.Customer;
 import com.allane.leasing.util.CustomerHelper;
@@ -18,6 +20,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "/application-test.properties")
 class AllaneCustomerIntegrationTests {
 
     @Autowired

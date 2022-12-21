9e1492e625596378a849f5e2552c178a1b9b2f53
Ahmad Shaman Virk <shaman.virk@gmail.com>
2022-12-21 05:44:06 +0100

    test: add integration tests for create customer and get paged customers

--- allane-leasing/src/test/java/com/allane/leasing/AllaneCustomerApplicationTests.java
+++ allane-leasing/src/test/java/com/allane/leasing/AllaneCustomerApplicationTests.java
@@ -2,8 +2,10 @@ package com.allane.leasing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "/application-test.properties")
 class AllaneLeasingApplicationTests {
 
 	@Test
--- allane-leasing/src/test/java/com/allane/leasing/integration/AllaneCustomersIntegrationTests.java
+++ allane-leasing/src/test/java/com/allane/leasing/integration/AllaneCustomersIntegrationTests.java
@@ -1,32 +1,76 @@
 package com.allane.leasing.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.allane.leasing.model.CustomerPageResponse;
+import com.allane.leasing.persistent.entity.CustomerEntity;
+import com.allane.leasing.persistent.repository.CustomerRepository;
+import com.allane.leasing.util.CustomerHelper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "/application-test.properties")
+@Transactional
 class AllaneCustomersIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("TestCase: Get all customers")
-    @Test
-    void getAllCustomers() throws Exception {
+    @Autowired
+    private CustomerRepository customerRepository;
 
-        mockMvc.perform(
-                get("/customers"))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-        .andReturn();
+    @DisplayName("TestCase: Get all customers - pagination")
+    @ParameterizedTest(name = "Given page: {0} and size: {1} then expect numberOfItemsOnCurrentPage: {2}, numberOfItems:{3}, numberOfPages: {4}")
+    @CsvSource({
+        "0, 5, 5, 5, 1",
+        "1, 5, 5, 10, 2"
+    })
+    void getAllCustomers(String page, String size, int numberOfItemsOnCurrentPage, int numberOfItems, int numberOfPages) throws Exception {
+        for (int i = 0; i < numberOfItems; i++) {
+            createCustomerEntity();
+        }
+
+        MvcResult mvcResult = mockMvc.perform(
+                get("/customers")
+                .param("page", page)
+                .param("size", size))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+
+        // validate pagination
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        CustomerPageResponse customerPageResponse = new ObjectMapper().registerModule(new JavaTimeModule())
+                .readerFor(new TypeReference<CustomerPageResponse>() {
+                }).readValue(responseBody);
+        assertEquals(numberOfItemsOnCurrentPage, customerPageResponse.getOverviewItems().size());
+        assertEquals(numberOfItems, customerPageResponse.getNumberOfItems());
+        assertEquals(numberOfPages, customerPageResponse.getNumberOfPages());
+    }
+
+    /*
+     * Private methods below are helpers methods for AllaneCustomersIntegrationTests only
+     * */
+    private CustomerEntity createCustomerEntity() {
+        CustomerEntity newCustomer = CustomerHelper.createCustomerEntity();
+        return customerRepository.save(newCustomer);
     }
 }

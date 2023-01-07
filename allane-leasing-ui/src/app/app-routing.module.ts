e5a4c94137d3fb9d43f731926f57bc6374ee26f6
Ahmad Shaman Virk <shaman.virk@gmail.com>
2023-01-07 21:43:13 +0100

    feat: add route for customer component

--- allane-leasing-ui/src/app/app-routing.module.ts
+++ allane-leasing-ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/all-customers', pathMatch: 'full' },
+  { path: 'all-customers', component: CustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

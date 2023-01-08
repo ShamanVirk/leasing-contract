73d50b620d9509f5d326e517b277c19edfb61c3a
Ahmad Shaman Virk <shaman.virk@gmail.com>
2023-01-08 21:45:09 +0100

    fix: remove needles annotation

--- allane-leasing/src/main/java/com/allane/leasing/security/configuration/WebConfigurer.java
+++ allane-leasing/src/main/java/com/allane/leasing/security/configuration/WebConfigurer.java
@@ -3,11 +3,9 @@ package com.allane.leasing.security.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfigurer implements WebMvcConfigurer {
     
     @Value("${cors.allowed.origins}")
@@ -16,5 +14,5 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(allowedOrigins);
-    }    
+    }
 }
